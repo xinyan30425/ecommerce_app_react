8ef9250fbda4c067431eaaec32430b205105fa84
xinyan30425 <xinyan.liu30425@gmail.com>
2023-12-10 22:33:12 -0500

    Fix npm run build

--- src/actions/userAction.jsx
+++ src/actions/userAction.jsx
@@ -80,7 +80,7 @@ export function signUp(signupData) {
 
 
     } catch (error) {
-  
+
       dispatch({ type: REGISTER_USER_FAIL, payload: error.message })
     }
 
@@ -96,15 +96,15 @@ export const load_UserProfile = () => async (dispatch) => {
 
     // Check if user data is available in session storage
     const userData = sessionStorage.getItem("user");
-    if (userData !== "undefined" && userData && userData !== undefined ) {
+    if (userData !== "undefined" && userData && userData !== undefined) {
       // Parse the user data from JSON format stored in session storage
       const user = JSON.parse(userData);
-       dispatch({ type: LOAD_USER_SUCCESS, payload: user });
+      dispatch({ type: LOAD_USER_SUCCESS, payload: user });
     } else {
 
       // If user data is not available in session storage, make a backend API call
-      const { data } = await axios.get(`api/v1/profile`);
-   
+      const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/profile`);
+
       dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
 
       // Save the user data to session storage for future use
@@ -150,10 +150,10 @@ export function updateProfile(userData) {
         config
       );
 
-     if(data.user !== undefined && data.user) {
-      sessionStorage.removeItem("user");
-       sessionStorage.setItem("user", JSON.stringify(data.user))
-     }
+      if (data.user !== undefined && data.user) {
+        sessionStorage.removeItem("user");
+        sessionStorage.setItem("user", JSON.stringify(data.user))
+      }
 
       dispatch({
         type: UPDATE_PROFILE_SUCCESS,
@@ -246,18 +246,18 @@ export const resetPassword = (token, passwords) => async (dispatch) => {
 
 
 // get All user Action --> admin 
-export const getAllUsers  = () =>async (dispatch) =>{
-     
+export const getAllUsers = () => async (dispatch) => {
+
   try {
 
-    dispatch({type : ALL_USERS_REQUEST})
+    dispatch({ type: ALL_USERS_REQUEST })
 
     const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/users`);
 
-    dispatch({ type: ALL_USERS_SUCCESS, payload: data.users});
-    
+    dispatch({ type: ALL_USERS_SUCCESS, payload: data.users });
+
   } catch (error) {
-      dispatch({type : ALL_USERS_FAIL , payload : error.message})
+    dispatch({ type: ALL_USERS_FAIL, payload: error.message })
   }
 
 }
@@ -266,48 +266,48 @@ export const getAllUsers  = () =>async (dispatch) =>{
 
 export const getUserDetails = (id) => async (dispatch) => {
   try {
-     dispatch({type : USER_DETAILS_REQUEST})
-         const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`);
-            dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
+    dispatch({ type: USER_DETAILS_REQUEST })
+    const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`);
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data.user });
 
   } catch (error) {
-     dispatch({ type: USER_DETAILS_FAIL , error : error.message});
+    dispatch({ type: USER_DETAILS_FAIL, error: error.message });
   }
 }
 
 // upadte user role ---> admin
 export const updateUser = (id, userData) => async (dispatch) => {
-       console.log(id);
+  console.log(id);
   try {
-     dispatch({type : UPDATE_USER_REQUEST})
+    dispatch({ type: UPDATE_USER_REQUEST })
 
 
-     const config  = {headers : {"Content-Type" : "application/json"}}
-     const { data } = await axios.put(
-       `${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`,userData,
-       config
-       
-     );
-     console.log(data);
+    const config = { headers: { "Content-Type": "application/json" } }
+    const { data } = await axios.put(
+      `${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`, userData,
+      config
+
+    );
+    console.log(data);
     dispatch({ type: UPDATE_USER_SUCCESS, payload: data.success });
 
   } catch (error) {
-      dispatch({type : UPDATE_USER_FAIL , payload : error.message} )
+    dispatch({ type: UPDATE_USER_FAIL, payload: error.message })
   }
 
 }
 
 // detele User ---> admin
 
-export const deleteUser  =(id) => async (dispatch) =>{
+export const deleteUser = (id) => async (dispatch) => {
   try {
-       dispatch({ type: DELETE_USER_REQUEST });
-       
-       const { data } = await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`);
-        dispatch({type : DELETE_USER_SUCCESS , payload : data})
+    dispatch({ type: DELETE_USER_REQUEST });
+
+    const { data } = await axios.delete(`${process.env.REACT_APP_API_BASE_URL}/api/v1/admin/user/${id}`);
+    dispatch({ type: DELETE_USER_SUCCESS, payload: data })
 
   } catch (error) {
-      dispatch({type : DELETE_USER_FAIL , payload : error.message})
+    dispatch({ type: DELETE_USER_FAIL, payload: error.message })
   }
 
 }
